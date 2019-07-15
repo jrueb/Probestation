5b70a4218fe899e7d330c630ce26e37a7ec260fc
Jonas Rübenach <jonas.ruebenach@desy.de>
2019-07-15 12:12:29 +0200

    Use multiple thread for device detection
    This speeds up detection as every timeout is running simultaneously.

--- gpib_detect.py
+++ gpib_detect.py
@@ -2,52 +2,72 @@
 
 from __future__ import absolute_import
 import visa
+import pyvisa.errors
 import logging
-import serial
-from sys import platform
+from multiprocessing.pool import ThreadPool
 
-class GPIBDetector ( object ) :
-	def __init__ ( self , useserial ) :
-		logger = logging.getLogger ( u'probestation.gpib_detect.GPIBDetector' )
-		resources = [""] * 1
-		try :
-			self._rm1 = visa.ResourceManager ( )
-			resources = self._rm1.list_resources ( )
-		except :
-			logger.debug ( u"  Failed to open ni-visa" )
-		try :
-			if useserial :
-			    self._rm2 = visa.ResourceManager ( u'@py' )
-			    resources += self._rm2.list_resources ( )
-		except :
-			logger.debug ( u"  Failed to open py-visa" )
-
+class GPIBDetector ( object ) :				
+	def __init__ ( self, useserial ) :
+		self.logger = logging.getLogger ( u'probestation.gpib_detect.GPIBDetector' )
+		self.pool = ThreadPool(processes=10)
+		self.useserial = useserial
 		self.identifiers = {}
-		logger.debug ( u"  Probing devices..." )
-		for res in resources:
-			logger.debug ( u"   Found %s", res )
-			if not ( res.startswith ( u"ASRL" ) or res.startswith ( u"GPIB" ) ) :
-				continue
 
-			dev = None
-			if res.startswith ( u"ASRL" ) and useserial :
-				logger.debug ( u"   Opening serial connection to %s", res )
-				try :
-					# 5000 msecs needed to catch slow devices...
-					dev = self._rm2.open_resource ( res, baud_rate = 19200, data_bits = 8, timeout = 5000 )
-				except :
-					logger.debug ( u"   Could not open serial connection to %s", res )
-			if res.startswith ( u"GPIB" ) :
-				logger.debug ( u"   Opening GPIB connection to %s", res )
-				try : 
-					dev = self._rm1.open_resource ( res )
-				except :
-					logger.debug ( u"   Could not open GPIB connection to %s", res )
-			if not ( dev == None ) :
-				idn = dev.query ( u"*IDN?" )
-				logger.debug ( u"   Got device identification: %s", idn )
-				self.identifiers[res] = idn
-				dev.close ( )
+		try :
+			rm = visa.ResourceManager ( )
+		except pyvisa.errors.Error :
+			self.logger.debug ( u"Failed to open ni-visa" )
+		else :
+			self.logger.debug ( u"Probing ni-visa devices..." )
+			self._probe_rm ( rm, False )
+		if useserial :
+			try :
+				rm = visa.ResourceManager ( "@py" )
+			except pyvisa.errors.Error :
+				self.logger.debug ( u"Failed to open pyvisa" )
+			else :
+				self.logger.debug ( u"Probing pyvisa devices..." )
+				self._probe_rm ( rm, True )
+		
+	def _probe_rm(self, rm, isserial) :
+		pool_results = []
+		for res in rm.list_resources ( ) :
+			if isserial and not res.startswith ( u"ASRL" ) :
+				continue
+			if not isserial and not res.startswith ( u"GPIB" ) :
+				continue
+				
+			result = self.pool.apply_async ( self._obtain_idn, ( rm, res, isserial ) )
+			pool_results.append ( result )
+			
+		for pool_result in pool_results :
+			try :
+				res, idn = pool_result.get ( )
+			except pyvisa.errors.Error :
+				if isserial:
+					logger.debug ( u"Could not open serial connection to %s", res )
+				else:
+					logger.debug ( u"Could not open GPIB connection to %s", res )
+			else:
+				if idn:
+					self.identifiers[res] = idn
+		
+	def _obtain_idn(self, rm, res, isserial) :
+		if isserial :
+			self.logger.debug ( u"Opening serial connection to %s", res )
+			# 5000 msecs needed to catch slow devices...
+			dev = rm.open_resource ( res, baud_rate = 19200, data_bits = 8, timeout = 5000 )
+		else :
+			self.logger.debug ( u"Opening GPIB connection to %s", res )
+			dev = rm.open_resource ( res )
+		idn = dev.query ( u"*IDN?" )
+		if idn:
+			self.logger.debug ( u"Got device identification: %s", idn )
+		else:
+			self.logger.debug ( u"Got no device identification" )
+		dev.close ( )
+		return res, idn
+		
 
 	def get_resname_for ( self, search ) :
 		for key, value in self.identifiers.items ( ) :
